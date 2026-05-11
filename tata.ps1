cf81be732ac831befd9001d79ad4b457c90460af
kosnaja12 <golf1234aagg@gmail.com>
2026-05-12 01:43:34 +0700

    Add files via upload

--- tata.ps1
+++ tata.ps1
@@ -1,6 +1,6 @@
 $baseUrl = "https://github.com/kosnaja12/cal/releases/download/tata"
 
-iwr "$baseUrl/THEGOLF.exe" -OutFile "$env:TEMP\THEGOLF.exe"
-iwr "$baseUrl/VMProtectSDK64.dll" -OutFile "$env:TEMP\VMProtectSDK64.dll"
+iwr "$baseUrl/THEGOLF.exe" -OutFile "$env:TEMP\THEGOLF.exe" -UseBasicParsing
+iwr "$baseUrl/VMProtectSDK64.dll" -OutFile "$env:TEMP\VMProtectSDK64.dll" -UseBasicParsing
 
-Start-Process "$env:TEMP\THEGOLF.exe" -Verb RunAs
+Start-Process "$env:TEMP\THEGOLF.exe"
